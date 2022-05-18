6e4b200fb5453352bb483bebcc44af92084a4369
Danny Vernovsky <dv696w@intl.att.com>
2022-05-18 15:38:07 +0300

    solved

--- src/logic/logic.test.ts
+++ src/logic/logic.test.ts
@@ -1,5 +1,57 @@
 import { calculateNextState } from './logic';
+import {generateNewMatrix} from '../api/api'
 describe('logic tests', () => {
+    it('every live cell with 2 neightbors shouls stay', ()=>{
+        const matrix = generateNewMatrix();
+        matrix[1][1] = true;
+        matrix[2][1] = true;
+        matrix[2][2] = true;
+
+        const nextStepMatrix = calculateNextState(matrix);
+        expect(nextStepMatrix[1][1]).toBeTruthy();
+    });
+
+    it('every live cell with 3 neightbors shouls stay', ()=>{
+        const matrix = generateNewMatrix();
+        matrix[1][1] = true;
+        matrix[2][1] = true;
+        matrix[2][2] = true;
+        matrix[1][2] = true;
+
+        const nextStepMatrix = calculateNextState(matrix);
+        expect(nextStepMatrix[1][1]).toBeTruthy();
+    });
+
+    it('every live cell with more than 3 neighbors should die', ()=>{
+        const matrix = generateNewMatrix();
+        matrix[1][1] = true;
+        matrix[2][1] = true;
+        matrix[2][2] = true;
+        matrix[1][2] = true;
+        matrix[0][2] = true;
+
+        const nextStepMatrix = calculateNextState(matrix);
+        expect(nextStepMatrix[1][1]).toBeFalsy();
+    });
+
+    it('every live cell with less than 2 neighbors should die', ()=>{
+        const matrix = generateNewMatrix();
+        matrix[13][18] = true;
+        
+        const nextStepMatrix = calculateNextState(matrix);
+        expect(nextStepMatrix[13][18]).toBeFalsy();
+    });
+
+    it('every dead cell with exactly 3 neighbors should become alive', ()=>{
+        const matrix = generateNewMatrix();
+        matrix[0][0] = true;
+        matrix[0][1] = true;
+        matrix[1][1] = true;
+
+        const nextStepMatrix = calculateNextState(matrix);
+        expect(nextStepMatrix[1][0]).toBeTruthy();
+    });
+
     // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
     // Any live cell with two or three live neighbours lives on to the next generation.
     // Any live cell with more than three live neighbours dies, as if by overpopulation.
--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,5 +1,48 @@
 import { Matrix } from "../api/api";
 
-export function calculateNextState(matrix: Matrix): Matrix{
-    return matrix;
+export function calculateNextState(matrix: Matrix): Matrix {
+    const nextStep = JSON.parse(JSON.stringify(matrix)) as Matrix;
+    for (const y of Object.keys(matrix)) {
+        for (const x of Object.keys(matrix[y])) {
+            const isCellAlive = matrix[y][x];
+            const livingNeighboursCount = getLivingNeighboursCount(x, y, matrix);
+            if (!isCellAlive && livingNeighboursCount === 3) {
+                nextStep[y][x] = true;
+            }
+
+            if (isCellAlive) {
+                if (livingNeighboursCount < 2 || livingNeighboursCount > 3) {
+                    nextStep[y][x] = false;
+                }
+            }
+        }
+    }
+    return nextStep;
 }
+
+const getLivingNeighboursCount = (x: string, y: string, matrix: Matrix) => {
+    let neighbours = 0;
+    vectors.forEach(v => {
+        const rowIndex = +y + v[0];
+        if (!matrix[rowIndex]) return;
+        const cellIndex = +x + v[1];
+        const cellValue = matrix[rowIndex][cellIndex];
+        if (cellValue) {
+            neighbours += 1;
+        }
+    });
+    return neighbours;
+}
+
+const vectors: [number, number][] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+]
+
+
